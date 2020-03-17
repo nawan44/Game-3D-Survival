1ed665646291266afd2b9acb575a01d228e83141
nawan44 <rachmat.d.gunawan@gmail.com>
2020-03-17 22:35:34 +0700

    final gen1

---
+++ Assets/Scripts/AmmoBox.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoBox : MonoBehaviour {
+
+    void OnTrigger(BoxCollider col) {
+        if (col.gameObject.tag == "Player") {
+            WeaponController.ammoMag = 90;
+        }
+    }
+}
